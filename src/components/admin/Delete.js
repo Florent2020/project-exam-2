01fdbd4a679770ff1111e9836de8bf681bec5e24
Florent2020 <60365705+Florent2020@users.noreply.github.com>
2021-07-14 02:22:35 +0200

    project-exam-2

--- src/components/admin/Delete.js
+++ src/components/admin/Delete.js
@@ -3,54 +3,60 @@ import PropTypes from "prop-types";
 import { useHistory } from "react-router-dom";
 import useAxios from "../../hooks/UseAxios";
 import { BASE_URL } from "../../constants/api";
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
 import { confirmAlert } from "react-confirm-alert";
-import 'react-confirm-alert/src/react-confirm-alert.css';
+import "react-confirm-alert/src/react-confirm-alert.css";
 
 export default function DeleteAccommodation({ id }) {
-	const [error, setError] = useState(null);
+  const [error, setError] = useState(null);
 
-	const http = useAxios();
-	const history = useHistory();
+  const http = useAxios();
+  const history = useHistory();
 
-	const url = BASE_URL + `/hotels/${id}`;
+  const url = BASE_URL + `/accommodations/${id}`;
 
-    function deleteHotel() {
-        confirmAlert({
-            title: "Are you sure you want to delete this???",
-            buttons: [
-              {
-                label: "yes",
-                onClick: () => handleDelete(),
-              },
-              {
-                label: "cancel",
-              },
-            ],
-          })
+  function deleteHotel() {
+    confirmAlert({
+      title: "Are you sure you want to delete this???",
+      buttons: [
+        {
+          label: "yes",
+          onClick: () => handleDelete(),
+        },
+        {
+          label: "cancel",
+        },
+      ],
+    });
+  }
+
+  async function handleDelete() {
+    try {
+      await http.delete(url);
+      history.push("/admin/accommodations");
+    } catch (error) {
+      setError(error);
     }
+  }
 
-	async function handleDelete() {
-		try {
-			await http.delete(url);
-			history.push("/admin/hotels");
-		} catch (error) {
-			setError(error);
-		}
-	}
+  return (
+    // <button type="button" className="delete" onClick={handleDelete}>
+    // 	{error ? "Error" : "Delete"}
+    // </button>
 
-	return (
-		// <button type="button" className="delete" onClick={handleDelete}>
-		// 	{error ? "Error" : "Delete"}
-		// </button>
-
-                <Button variant="warning" type="submit" name="delete" className="delete" onClick={deleteHotel}>
-                    <i className="fas fa-trash"></i>
-                    {error ? "Error" : "Delete"}
-                </Button>
-            );
+    <Button
+      variant="warning"
+      type="submit"
+      name="delete"
+      className="delete"
+      onClick={deleteHotel}
+    >
+      <i className="fas fa-trash"></i>
+      {error ? "Error" : "Delete"}
+    </Button>
+  );
 }
 
 DeleteAccommodation.propTypes = {
-	id: PropTypes.number.isRequired,
+  id: PropTypes.number.isRequired,
 };
