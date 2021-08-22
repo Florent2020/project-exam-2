bc0274de62f6d63105573260013fa4ae74323e8a
Florent2020 <60365705+Florent2020@users.noreply.github.com>
2021-08-22 16:30:30 +0200

    color changed

--- src/hooks/UseAxios.js
+++ src/hooks/UseAxios.js
@@ -2,20 +2,21 @@ import { useContext } from "react";
 import axios from "axios";
 import AuthContext from "../context/AuthContext";
 
+// const url = process.env.REACT_APP_BASE_URL;
 const url = process.env.REACT_APP_BASE_URL;
 
 export default function useAxios() {
-	const [auth] = useContext(AuthContext);
+  const [auth] = useContext(AuthContext);
 
-	const apiClient = axios.create({
-		baseURL: url,
-	});
+  const apiClient = axios.create({
+    baseURL: url,
+  });
 
-	apiClient.interceptors.request.use(function (config)  {
-		const token = auth.token;
-		config.headers.Authorization = token ? `Bearer ${token}` : "";
-		return config;
-	});
+  apiClient.interceptors.request.use(function (config) {
+    const token = auth.token;
+    config.headers.Authorization = token ? `Bearer ${token}` : "";
+    return config;
+  });
 
-	return apiClient;
+  return apiClient;
 }
