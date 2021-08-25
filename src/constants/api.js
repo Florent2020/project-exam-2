f178fd05e1fd69dcfde935319b777e1db983a6f3
Florent2020 <60365705+Florent2020@users.noreply.github.com>
2021-08-25 13:13:48 +0200

    project-exam-2

--- src/constants/api.js
+++ src/constants/api.js
@@ -2,6 +2,6 @@
 // export const BASE_URL = process.env.REACT_APP_BASE_URL;
 // export const TOKEN_PATH = "/auth/local";
 
-export const TOKEN_PATH = "path/to/live/api";
+export const TOKEN_PATH = "/path/to/live/api";
 
 export const BASE_URL = "https://pure-mountain-97036.herokuapp.com";
